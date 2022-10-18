5544909169f1e9e792f77912b760560763ab3f44
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-18 19:50:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # this script assumes that repository https://github.com/mathebuddy/mathebuddy-downloads is cloned next 
 # to this repository (https://github.com/mathebuddy/mathebuddy-vscode-lang-ext)
-vsce package --out "../mathebuddy-downloads/vscode-plugins/"
+vsce package --out "../mathebuddy-downloads/vscode-extensions/"
